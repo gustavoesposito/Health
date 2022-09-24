4435eb86593b9c2823acaa53e1b95817d717f1ce
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-24 20:38:39 -0300

    att

--- app_healht/src/components/Form/index.js
+++ app_healht/src/components/Form/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React {useState} from "react";
+
 import {View, Text, TextInput, Button} from 'react-native';
 import ResultImc from "./ResultIMC";
+import { useState } from 'react';
   
 export default function Form(){
+
+  
+
     return(
 <View>
      <View>
