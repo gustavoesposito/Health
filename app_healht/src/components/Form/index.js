65553e6a1912347a57aac4397fe04a61f4227d27
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-25 13:53:06 -0300

    att

--- app_healht/src/components/Form/index.js
+++ app_healht/src/components/Form/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {View, Text, TextInput,TouchableOpacity,   } from 'react-native';
+import {View, Text, TextInput,TouchableOpacity } from 'react-native';
 import ResultImc from "./ResultIMC";
 import styles from "./style";
    
