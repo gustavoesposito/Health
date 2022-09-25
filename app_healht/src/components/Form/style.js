0b696d1d10c3eba7eb58569e7ae95e1a6ab29963
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-24 23:08:11 -0300

    att

--- app_healht/src/components/Form/style.js
+++ app_healht/src/components/Form/style.js
@@ -1,4 +1,5 @@
-import { StyleSheet } from "react-native ";
+import { StyleSheet  } from 'react-native';
+ 
 
 const styles = StyleSheet.create({
     formContent: {
@@ -30,6 +31,8 @@ const styles = StyleSheet.create({
         margin:12,
         paddingLeft:10,
     }
-});
-
+    });
 export default styles
+ 
+
+ 
