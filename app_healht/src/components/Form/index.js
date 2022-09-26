948c249a1bcffdaf86fcb725d9f4bf7e9e4e29ef
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-25 22:00:02 -0300

    att

--- app_healht/src/components/Form/index.js
+++ app_healht/src/components/Form/index.js
@@ -114,11 +114,12 @@ export default function Form(){
   data={imcList.reverse()}
   renderItem={({item}) => {
     return(
-      <Text> 
-      <Text>
+      <Text style={styles.resultImcItem}> 
+      <Text style={styles.textResultItemList}>
          Resultado IMC =
-         {item.imc}
+          
       </Text>
+      {item.imc}
       </Text>
     )
   }}
